0a45234ff3231ec202bb17db00fd9003590d6c44
xiaohuochai <121631835@qq.com>
2023-02-16 11:04:39 +0800

    change clipboard.lua to clipboardTool.spoon

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 require('music')
-require('clipboard')
 require('headphone')
+
+-- require ClipboardTool
+hs.loadSpoon('ClipboardTool')
+spoon.ClipboardTool.paste_on_select = true
+spoon.ClipboardTool.show_in_menubar = false
+spoon.ClipboardTool:start()
+hs.hotkey.bind({"cmd", "shift","ctrl"}, 'v', function()
+  spoon.ClipboardTool:showClipboard()
+end)
