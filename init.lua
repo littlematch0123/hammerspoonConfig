4a17aa27e1dd3456fc782a2839063b127fb88cd1
xiaohuochai <121631835@qq.com>
2023-02-28 16:20:32 +0800

    delete the clipboard functionality

--- init.lua
+++ init.lua
@@ -1,11 +1,3 @@
 require('music')
 require('headphone')
 
--- require ClipboardTool
-hs.loadSpoon('ClipboardTool')
-spoon.ClipboardTool.paste_on_select = true
-spoon.ClipboardTool.show_in_menubar = false
-spoon.ClipboardTool:start()
-hs.hotkey.bind({"cmd", "shift","ctrl"}, 'v', function()
-  spoon.ClipboardTool:showClipboard()
-end)
